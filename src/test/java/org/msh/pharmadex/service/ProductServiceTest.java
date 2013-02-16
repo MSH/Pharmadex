3a9c3c183bef10fb356e730a4e0366d84c1172f3
usrivastava <usrivastava@24bb7120-ba24-2248-a177-eda5b6c43647>
2013-02-16 20:29:55 +0000

    Add search funtionality
    git-svn-id: https://www.etbmanager.org/svn/pharmadex/pharmadex/trunk@10 24bb7120-ba24-2248-a177-eda5b6c43647

---
+++ src/test/java/org/msh/pharmadex/service/ProductServiceTest.java
@@ -0,0 +1,30 @@
+package org.msh.pharmadex.service;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: utkarsh
+ * Date: 2/16/13
+ * Time: 3:21 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ProductServiceTest {
+
+
+    public void setUp() throws Exception {
+//        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testFindProductByFilter() throws Exception {
+        Assert.assertEquals(true, false);
+
+    }
+}
