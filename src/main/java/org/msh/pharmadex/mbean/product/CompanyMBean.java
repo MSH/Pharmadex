03d84e43161656eb7a0ddacaa3da65ee742c2f7b
usrivastava <usrivastava@24bb7120-ba24-2248-a177-eda5b6c43647>
2013-02-24 13:20:51 +0000

    Add search funtionality
    git-svn-id: https://www.etbmanager.org/svn/pharmadex/pharmadex/trunk@16 24bb7120-ba24-2248-a177-eda5b6c43647

--- src/main/java/org/msh/pharmadex/mbean/product/CompanyMBean.java
+++ src/main/java/org/msh/pharmadex/mbean/product/CompanyMBean.java
@@ -3,13 +3,13 @@ package org.msh.pharmadex.mbean.product;
 import org.msh.pharmadex.domain.Company;
 import org.msh.pharmadex.domain.Country;
 import org.msh.pharmadex.domain.enums.CompanyType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * Author: usrivastava
@@ -17,36 +17,37 @@ import java.util.List;
 @Component
 @Scope("request")
 public class CompanyMBean {
+    private static final Logger logger = LoggerFactory.getLogger(CompanyMBean.class);
 
     @Autowired
     RegHomeMbean regHomeMbean;
 
-    private CompanyType[] companyType;
     private Company selectedCompany;
 
+
     @PostConstruct
-    private void init(){
-        System.out.println("------------------------------------------------------------------------------------");
-        System.out.println(" Company Bean instantiating ");
-        System.out.println("------------------------------------------------------------------------------------");
+    private void init() {
+        logger.debug("------------------------------------------------------------------------------------");
+        logger.debug(" Company Bean instantiating ");
+        logger.debug("------------------------------------------------------------------------------------");
 
         selectedCompany = new Company();
         selectedCompany.getAddress().setCountry(new Country());
         selectedCompany.setCompanyType(CompanyType.MANUFACTURER);
     }
 
-    public String addCompany(){
+    public String addCompany() {
         selectedCompany.setProduct(regHomeMbean.getProduct());
         regHomeMbean.getCompanies().add(selectedCompany);
         return null;
     }
 
-    public String removeCompany(){
+    public String removeCompany() {
         regHomeMbean.getCompanies().remove(selectedCompany);
         return null;
     }
 
-    public String cancelAdd(){
+    public String cancelAdd() {
         return null;
     }
 
@@ -58,12 +59,5 @@ public class CompanyMBean {
         this.selectedCompany = selectedCompany;
     }
 
-    public List<CompanyType> getCompanyType() {
-        return Arrays.asList(CompanyType.values());
 
-    }
-
-    public void setCompanyType(CompanyType[] companyType) {
-        this.companyType = companyType;
-    }
 }
