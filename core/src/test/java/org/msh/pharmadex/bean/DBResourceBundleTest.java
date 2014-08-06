cde3b98092294fb39d0ca4f322a690a80969f21a
USrivastava <USrivastava@24bb7120-ba24-2248-a177-eda5b6c43647>
2014-08-06 17:13:29 +0000

    git-svn-id: https://www.etbmanager.org/svn/pharmadex/pharmadex/trunk@126 24bb7120-ba24-2248-a177-eda5b6c43647

---
+++ core/src/test/java/org/msh/pharmadex/bean/DBResourceBundleTest.java
@@ -0,0 +1,30 @@
+package org.msh.pharmadex.bean;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.msh.pharmadex.auth.DBResourceBundle;
+import org.msh.pharmadex.mbean.DBResourceMbn;
+import org.msh.pharmadex.model.Task;
+import org.msh.pharmadex.util.RegistrationUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@ContextConfiguration("/test-context.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class DBResourceBundleTest {
+	
+	@Autowired
+    DBResourceMbn dBResourceMbn;
+
+	@Test
+	public void shouldGetCorrectMessage() {
+        Assert.assertEquals(true, true);
+        String newvalue = RegistrationUtil.formatString("New medicine, old medicine. /why (do) we");
+        System.out.println("Formatted value == "+newvalue);
+        Assert.assertEquals(true, true);
+	}
+	
+
+}
