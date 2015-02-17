8a2c5c9412837ca63e0f934dfb34797d9b6b41a6
USrivastava <USrivastava@24bb7120-ba24-2248-a177-eda5b6c43647>
2015-02-17 23:08:21 +0000

    git-svn-id: https://www.etbmanager.org/svn/pharmadex/pharmadex/trunk@298 24bb7120-ba24-2248-a177-eda5b6c43647

--- core/src/main/java/org/msh/pharmadex/auth/LogoutListener.java
+++ core/src/main/java/org/msh/pharmadex/auth/LogoutListener.java
@@ -2,6 +2,7 @@ package org.msh.pharmadex.auth;
 
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.session.SessionDestroyedEvent;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
@@ -20,15 +21,13 @@ public class LogoutListener implements ApplicationListener<SessionDestroyedEvent
     @Override
     public void onApplicationEvent(SessionDestroyedEvent sessionDestroyedEvent) {
         {
-            ELContext elContext = FacesContext.getCurrentInstance().getELContext();
+//            ELContext elContext = FacesContext.getCurrentInstance().getELContext();
             List<SecurityContext> lstSecurityContext = sessionDestroyedEvent.getSecurityContexts();
             UserDetails ud = null;
             for (SecurityContext securityContext : lstSecurityContext) {
                 ud = (UserDetails) securityContext.getAuthentication().getPrincipal();
-                UserSession userSession
-                        = (UserSession) FacesContext.getCurrentInstance().getApplication()
-                        .getELResolver().getValue(elContext, null, "userSession");
-                userSession.registerLogout();
+                SecurityContextHolder.clearContext();
+//                response.sendRedirect(request.getContextPath() + "/home.faces?faces-redirect=true");
             }
             System.out.println("-------------- Username == " + ud.getUsername());
         }
