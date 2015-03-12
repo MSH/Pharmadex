bd2f92efacc714c9d15def9621caf9316dbfa004
USrivastava <USrivastava@24bb7120-ba24-2248-a177-eda5b6c43647>
2015-03-12 12:56:20 +0000

    02172015 UT Implementing changes for review functionality
    git-svn-id: https://www.etbmanager.org/svn/pharmadex/pharmadex/trunk@311 24bb7120-ba24-2248-a177-eda5b6c43647

--- core/src/main/java/org/msh/pharmadex/dao/ResourceBundleDAO.java
+++ core/src/main/java/org/msh/pharmadex/dao/ResourceBundleDAO.java
@@ -1,18 +1,13 @@
 package org.msh.pharmadex.dao;
 
-import org.hibernate.exception.ConstraintViolationException;
-import org.msh.pharmadex.domain.Applicant;
 import org.msh.pharmadex.domain.ResourceBundle;
-import org.msh.pharmadex.domain.User;
-import org.msh.pharmadex.domain.enums.ApplicantState;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import java.io.Serializable;
-import java.util.List;
 import java.util.Locale;
 
 /**
@@ -30,7 +25,11 @@ public class ResourceBundleDAO implements Serializable {
     EntityManager entityManager;
 
     public ResourceBundle findResourceBundleByLocale(Locale locale){
-        return (ResourceBundle) entityManager.createNamedQuery(ResourceBundle.QUERYNAME_FIND_BY_LOCALE).setParameter("locale", ""+locale).getSingleResult();
+        try {
+            return (ResourceBundle) entityManager.createNamedQuery(ResourceBundle.QUERYNAME_FIND_BY_LOCALE).setParameter("locale", "" + locale).getSingleResult();
+        } catch (NoResultException nre) {
+            return (ResourceBundle) entityManager.createNamedQuery(ResourceBundle.QUERYNAME_FIND_BY_LOCALE).setParameter("locale", "en").getSingleResult();
+        }
     }
 
     public void save(ResourceBundle resourceBundle){
