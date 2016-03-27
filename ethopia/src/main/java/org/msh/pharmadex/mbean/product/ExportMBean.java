62d05169c47d10cf829c987c70523516d4a09eab
okurasov <okurasov@24bb7120-ba24-2248-a177-eda5b6c43647>
2016-03-27 11:51:09 +0000

    git-svn-id: https://www.etbmanager.org/svn/pharmadex/pharmadex/trunk@519 24bb7120-ba24-2248-a177-eda5b6c43647

---
+++ ethopia/src/main/java/org/msh/pharmadex/mbean/product/ExportMBean.java
@@ -0,0 +1,160 @@
+package org.msh.pharmadex.mbean.product;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.*;
+import org.msh.pharmadex.domain.*;
+
+import org.msh.pharmadex.service.ExportService;
+import org.msh.pharmadex.service.LicenseHolderService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import java.io.*;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by wing on 23.03.2016.
+ */
+
+
+@ManagedBean
+@ViewScoped
+
+public class ExportMBean implements Serializable {
+    private String filename;
+    public static Workbook wb;
+    private static java.util.regex.Pattern numeric = java.util.regex.Pattern.compile("\\d*");
+    private static int success=0;
+    private static int failure=0;
+    private static int total=0;
+
+    @ManagedProperty(value = "#{exportService}")
+    private ExportService exportService ;
+
+    public void setExportService(ExportService exportService){
+     this.exportService=exportService;
+ }
+
+    public ExportService getExportService() {
+        return exportService;
+    }
+
+
+    @ManagedProperty(value = "#{licenseHolderService}")
+    private LicenseHolderService licenseHolderService;
+
+    public LicenseHolderService getLicenseHolderService() {
+        return licenseHolderService;
+    }
+
+    public void setLicenseHolderService(LicenseHolderService licenseHolderService) {
+        this.licenseHolderService = licenseHolderService;
+    }
+
+
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+
+    public String startExport(){
+        FacesContext context = FacesContext.getCurrentInstance();
+        if (filename==null) return "";
+        File f =new File(filename) ;
+        if (f.isFile()) try {
+            wb = WorkbookFactory.create(new FileInputStream(filename));
+            Sheet sheet = wb.getSheetAt(0);
+            if (sheet == null) return "";
+            boolean res;
+            for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+                res = exportService.importRow(sheet.getRow(i));
+                if (res) success++;
+                else failure++;
+            }
+            Cell cell = wb.getSheetAt(0).getRow(1).getCell(1);
+            CellStyle style = wb.createCellStyle();
+            style.setFillForegroundColor(IndexedColors.DARK_RED.getIndex());
+            style.setFillBackgroundColor(IndexedColors.DARK_GREEN.getIndex());
+            style.setFillPattern(CellStyle.SOLID_FOREGROUND);
+            cell.setCellStyle(style);
+
+            File outf = new File("C:/Temp/res.xls");
+            FileOutputStream out = new FileOutputStream(outf);
+            wb.write(out);
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InvalidFormatException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+
+
+    /*private void importRow(Row row) {
+
+        Product prod=new Product();
+        LicenseHolder lic=new LicenseHolder();
+        Applicant a=new Applicant();
+        ProdApplications pa=new ProdApplications();
+        Address addr=new Address();
+
+        Cell cell = row.getCell(0);   //Presentation -
+        if (cell!=null) prod.setPackSize(cell.getStringCellValue());
+
+        cell = row.getCell(1);  //Route Of Admin  catalog   in table adminroute
+        if (cell!=null)
+         prod.setAdminRoute(exportService.getAdminRouteAcc(cell.getStringCellValue()));
+        cell = row.getCell(2);  //Therapeutic Group    catalog  in table adminroute
+        if (cell!=null)
+        //temporary    prod.setAtcs(exportService.getAtcAcc(cell.getStringCellValue()));
+        cell = row.getCell(3);  //Indication   catalog in table pharmSlassif
+        if (cell!=null)
+         prod.setPharmClassif(exportService.getpharmSlassifAcc(cell.getStringCellValue()));
+        //col 4 classification is empty
+        cell = row.getCell(5);//Brand Name
+        if (cell!=null) prod.setProdName(cell.getStringCellValue());
+        cell = row.getCell(6);  //Generic Name
+        if (cell!=null)  prod.setGenName(cell.getStringCellValue());
+        cell = row.getCell(7);  //Dose strength
+        if (cell!=null)  prod.setDosStrength(cell.getStringCellValue());
+        cell = row.getCell(8);  //Dosage Form
+          prod.setDosForm(exportService.getDosFormAcc(cell.getStringCellValue()));
+        cell = row.getCell(9);//prod_desc
+        if (cell!=null)  prod.setDosForm(exportService.getDosFormAcc(cell.getStringCellValue()));
+        cell = row.getCell(10);  //shelf_life(Months)
+        if (cell!=null)prod.setShelfLife(cell.getStringCellValue());
+        cell = row.getCell(11);  //Licence Holder/manufacturer
+        if (cell!=null) lic.setName(cell.getStringCellValue());
+        cell = row.getCell(12); //Local Agent
+        if (cell!=null) lic.setFirstAgent(cell.getStringCellValue());
+
+         cell = row.getCell(13); //Registration Date
+       //if (cell!=null)pa.setRegistrationDate(cell.getDateCellValue());
+        cell = row.getCell(14); //RExpiry Date
+        //if (cell!=null) pa.setRegExpiryDate(cell.getDateCellValue());
+         cell = row.getCell(15);//Manufacturer/Actual site/
+        if (cell!=null) a.setAppName(cell.getStringCellValue());
+        cell = row.getCell(16); //Country of Origin
+        if (cell!=null) {
+
+            addr.setCountry(exportService.getCountry(cell.getStringCellValue()));
+            lic.setAddress(addr);
+        }
+
+
+    }*/
+
+
+}
---
+++ ethopia/src/main/java/org/msh/pharmadex/service/ExportService.java
@@ -0,0 +1,240 @@
+package org.msh.pharmadex.service;
+
+import org.apache.poi.ss.usermodel.*;
+
+
+import org.msh.pharmadex.dao.CountryDAO;
+import org.msh.pharmadex.dao.CustomLicHolderDAO;
+import org.msh.pharmadex.dao.CustomDictionaryDAO;
+import org.msh.pharmadex.dao.ProdApplicationsDAO;
+import org.msh.pharmadex.domain.*;
+
+import org.msh.pharmadex.utils.ExcelTools;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
+import java.util.*;
+
+/**
+ * Created by wing on 24.03.2016.
+ */
+//@Service ("exportService")
+@Service
+public class ExportService implements Serializable {
+    @Autowired
+    ProdApplicationsDAO prodApplicationsDAO;
+
+    @Autowired
+    CountryDAO countryDAO;
+
+    @Autowired
+    private CustomLicHolderDAO customLicHolderDAO;
+
+    //@ManagedProperty (value = "#{dosageFormService}")
+    @Autowired
+    CustomDictionaryDAO dictionaryDAO;
+    //private DosageFormService dosageFormService;
+
+
+    /*public DosageFormService getDosageFormService() {
+        return dosageFormService;
+    }
+
+    public void setDosageFormService(DosageFormService dosageFormService) {
+        this.dosageFormService = dosageFormService;
+    }*/
+
+    Row currrow;
+    private  boolean errorDetected;
+
+    public boolean importRow(Row row) {
+        currrow=row;
+        errorDetected=false;
+        Product prod=new Product();
+        LicenseHolder lic=new LicenseHolder();
+        Applicant a=new Applicant();
+        ProdApplications pa=new ProdApplications();
+        Address addr=new Address();
+
+        Cell cell = row.getCell(0);   //Presentation -
+        if (cell!=null) prod.setPackSize(cell.getStringCellValue());
+
+        cell = row.getCell(1);  //Route Of Admin  catalog   in table adminroute
+        if (cell!=null)
+            prod.setAdminRoute(getAdminRouteAcc(cell.getStringCellValue()));
+        cell = row.getCell(2);  //Therapeutic Group    catalog  in table adminroute
+        if (cell!=null)
+             prod.setAtcs(getAtcList(cell.getStringCellValue()));
+            cell = row.getCell(3);  //Indication   catalog in table pharmSlassif
+        if (cell!=null)
+            prod.setPharmClassif(getpharmSlassifAcc(cell.getStringCellValue()));
+        //col 4 classification is empty
+        cell = row.getCell(5);//Brand Name
+        if (cell!=null) prod.setProdName(cell.getStringCellValue());
+        cell = row.getCell(6);  //Generic Name
+        if (cell!=null)  prod.setGenName(cell.getStringCellValue());
+        cell = row.getCell(7);  //Dose strength
+        if (cell!=null)  prod.setDosStrength(cell.getStringCellValue());
+        cell = row.getCell(8);  //Dosage Form
+        prod.setDosForm(getDosFormAcc(cell.getStringCellValue()));
+        cell = row.getCell(9);//prod_desc
+        if (cell!=null)  prod.setDosForm(getDosFormAcc(cell.getStringCellValue()));
+        cell = row.getCell(10);  //shelf_life(Months)
+        if (cell!=null)prod.setShelfLife(cell.getStringCellValue());
+        cell = row.getCell(11);  //Licence Holder/manufacturer
+        if (cell!=null) lic=findLicHolderByName(cell.getStringCellValue());
+        cell = row.getCell(12); //Local Agent
+        if (cell!=null) lic.setFirstAgent(cell.getStringCellValue());
+
+        cell = row.getCell(13); //Registration Date
+        //if (cell!=null)pa.setRegistrationDate(cell.getDateCellValue());
+        cell = row.getCell(14); //RExpiry Date
+        //if (cell!=null) pa.setRegExpiryDate(cell.getDateCellValue());
+        cell = row.getCell(15);//Manufacturer/Actual site/
+        if (cell!=null) a.setAppName(cell.getStringCellValue());
+        cell = row.getCell(16); //Country of Origin
+        if (cell!=null) {
+
+            addr.setCountry(getCountry(cell.getStringCellValue()));
+            lic.setAddress(addr);
+        }
+        if (errorDetected) return false;
+        return addToDatabase(prod, lic, a, pa, addr);
+
+    }
+
+    public boolean addToDatabase(Product prod, LicenseHolder lic, Applicant a, ProdApplications pa, Address addr) {
+        return true;
+    }
+
+    // /check dictionary methods
+    public LicenseHolder findLicHolderByName(String s){
+        LicenseHolder  r=customLicHolderDAO.findLicHolderByName(s);
+
+        if (r!=null)return r;
+        r=new LicenseHolder();
+        r.setName(s);
+        //ExcelTools.setCellBackground(currrow.getCell(16), IndexedColors.GREY_40_PERCENT.getIndex());
+
+        return r;
+    }
+    public DosageForm getDosFormAcc(String s) {
+        DosageForm  r=dictionaryDAO.findDosFormByName(s);
+        if (r!=null)return r;
+        ExcelTools.setCellBackground(currrow.getCell(9), IndexedColors.GREY_40_PERCENT.getIndex());
+        errorDetected=true;
+        return r;
+    }
+
+    public AdminRoute getAdminRouteAcc(String s) {
+        AdminRoute r = dictionaryDAO.findAdminRouteByName(s);
+        if (r!=null)return r;
+        ExcelTools.setCellBackground(currrow.getCell(2), IndexedColors.GREY_40_PERCENT.getIndex());
+        errorDetected=true;
+        return r;
+    }
+
+    public List<Atc> getAtcList(String s) {
+        List<Atc> res=new ArrayList<Atc>();
+        String[]allnames= new String[10];
+        allnames=s.split(", ",10);
+        for(int i=0;i<= allnames.length;i=i+2) {
+            if (!allnames[i].equalsIgnoreCase("")) {
+                Atc atc = dictionaryDAO.findAtsbyCode(allnames[i]);
+                if (atc != null) res.add(atc);
+                else {
+ //                   ExcelTools.setCellBackground(currrow.getCell(9), IndexedColors.GREY_40_PERCENT.getIndex());
+                    errorDetected = true;
+                    return null;
+                }
+            }
+        }
+        return res;
+    }
+
+    public PharmClassif getpharmSlassifAcc(String s) {
+        PharmClassif r = dictionaryDAO.findPharmSlassifByName(s);
+        if (r!=null)return r;
+        ExcelTools.setCellBackground(currrow.getCell(3), IndexedColors.GREY_40_PERCENT.getIndex());
+        errorDetected=true;
+        return r;
+    }
+
+
+
+
+    public Country getCountry(String s) {
+        Country r=countryDAO.findCountryByName(s);
+        if (r!=null)return r;
+            ExcelTools.setCellBackground(currrow.getCell(16), IndexedColors.GREY_40_PERCENT.getIndex());
+       errorDetected=true;
+        return r;
+    }
+
+
+/*    public static void errorToLog(Workbook wb, ErrorCell errorCell){
+        if (errorCell==null) return;
+        RequestService.logEvent("Імпорт", errorCell.getErrorMsg(), author);
+        try {
+            CreationHelper factory = ImportXL.wb.getCreationHelper();
+            ClientAnchor anchor = factory.createClientAnchor();
+            //CellReference cr = new CellReference(errorCell.getCellName());
+            Row row = wb.getSheet(errorCell.getTabName()).getRow(errorCell.getRowNo());
+            Cell cell = row.getCell(errorCell.getColumnNo());
+            if (cell==null){
+                cell = row.createCell(errorCell.getColumnNo());
+                cell.setCellValue(" ");
+            }
+            anchor.setAnchorType(1);
+            anchor.setCol1(cell.getColumnIndex());
+            anchor.setCol2(cell.getColumnIndex()+1);
+            anchor.setRow1(row.getRowNum()-1);
+            anchor.setRow2(row.getRowNum()+3);
+            Drawing drawing = cell.getSheet().createDrawingPatriarch();
+            Comment comment = cell.getCellComment();
+            if (comment==null)
+                comment = drawing.createCellComment(anchor);
+            RichTextString str = comment.getString();
+            if (str==null)
+                str = factory.createRichTextString(errorCell.getErrorMsg());
+            else{
+                if (!Tools.isEmptyStr(str.getString())){
+                    String addStr = str.getString() + ";" + errorCell.getErrorMsg();
+                    str = factory.createRichTextString(addStr);
+                }else
+                    str = factory.createRichTextString(errorCell.getErrorMsg());
+            }
+            comment.setVisible(Boolean.FALSE);
+            comment.setString(str);
+            ExcelTools.setCellBackground(cell, IndexedColors.GREY_40_PERCENT.getIndex());
+            cell.setCellComment(comment);
+            //запишем файл с изменениями в специальную папку
+            String outFileName=getOutFileName();
+            FileOutputStream out = new FileOutputStream(outFileName);
+            wb.write(out);
+            out.close();
+        } catch (FileNotFoundException e) {
+            RequestService.logEvent("Помилка", "Неможливо відкрити електронну таблицю для аналізу помилок", author);
+        } catch (IOException e) {
+            RequestService.logEvent("Помилка", "Неможливо записати файл з аналізом помилок", author);		}
+        catch (IllegalStateException e){
+            // nothing to do - попытка вставить второй комментарий
+        } catch (IllegalArgumentException e){
+            // nothing to do  - попытка вставить второй комментарий
+        }
+    }
+
+    public static void errorsToLog(Workbook wb, List<ErrorCell> errorCells, Clerk author){
+        if (errorCells.size()==0) return;
+        //записали в лог базы
+        for(int i=0;i<errorCells.size();i++){
+            ErrorCell ec = errorCells.get(i);
+            RequestService.logEvent("Імпорт", ec.getErrorMsg(), author);
+        }
+        //теперь делаем отметки в файле Excel (в местах ошибок)
+        for(int i=0;i<errorCells.size();i++){
+            errorToLog(wb, errorCells.get(i), author);
+        }
+    }*/
+}
