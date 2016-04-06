18e96bc15c451ce5046d37e59f84d3a617c19e27
okurasov <okurasov@24bb7120-ba24-2248-a177-eda5b6c43647>
2016-04-06 06:13:42 +0000

    git-svn-id: https://www.etbmanager.org/svn/pharmadex/pharmadex/trunk@522 24bb7120-ba24-2248-a177-eda5b6c43647

--- ethopia/src/main/java/org/msh/pharmadex/dao/CustomDictionaryDAO.java
+++ ethopia/src/main/java/org/msh/pharmadex/dao/CustomDictionaryDAO.java
@@ -1,8 +1,5 @@
 package org.msh.pharmadex.dao;
-import org.msh.pharmadex.domain.Atc;
-import org.msh.pharmadex.domain.AdminRoute;
-import org.msh.pharmadex.domain.DosageForm;
-import org.msh.pharmadex.domain.PharmClassif;
+import org.msh.pharmadex.domain.*;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -50,6 +47,36 @@ public class CustomDictionaryDAO {
         try{
             return (PharmClassif) entityManager.createQuery("select f from PharmClassif f where f.name = :aName ")
                     .setParameter("aName", name).getSingleResult();
+        } catch (Exception ex) {
+            return null;
+        }
+    }
+
+
+public DosUom findDosUomByName(String res) {
+    try{
+        return (DosUom) entityManager.createQuery("select u from DosUom u where u.uom = :name ")
+                .setParameter("name", res).getSingleResult();
+    } catch (Exception ex) {
+        return null;
+    }
+
+    }
+    public Applicant findApplicantByName(String res ) {
+        try{
+         List<Applicant> ap= (List<Applicant>)entityManager.createQuery("select a from Applicant a where a.appName like :name ")
+                .setParameter("name", res).getResultList();
+            return  ap.get(0);
+      } catch (Exception ex) {
+                   return null;
+      }
+    }
+
+    public Company findCompanyByName(String res ) {
+        try{
+            List<Company> ap= (List<Company>)entityManager.createQuery("select c from Company c where c.companyName like :name ")
+                    .setParameter("name", res).getResultList();
+            return  ap.get(0);
         } catch (Exception ex) {
             return null;
         }
