3b67d31ad284bdbefb7632fc207c43d3be9820e1
okurasov <okurasov@24bb7120-ba24-2248-a177-eda5b6c43647>
2016-04-25 15:21:55 +0000

    Failed commit: Default
    git-svn-id: https://www.etbmanager.org/svn/pharmadex/pharmadex/trunk@571 24bb7120-ba24-2248-a177-eda5b6c43647

--- ethopia/src/main/java/org/msh/pharmadex/mbean/product/TestReportsBn.java
+++ ethopia/src/main/java/org/msh/pharmadex/mbean/product/TestReportsBn.java
@@ -2,7 +2,9 @@ package org.msh.pharmadex.mbean.product;
 
 import net.sf.jasperreports.engine.JRException;
 import org.msh.pharmadex.domain.ProdApplications;
+import org.msh.pharmadex.domain.SuspDetail;
 import org.msh.pharmadex.service.ProdApplicationsService;
+import org.msh.pharmadex.service.SuspendService;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
@@ -10,6 +12,7 @@ import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 /**
  * Created by Odissey on 19.04.2016.
@@ -23,6 +26,16 @@ public class TestReportsBn {
     @ManagedProperty(value = "#{prodApplicationsService}")
     private ProdApplicationsService prodApplicationsService;
 
+    public SuspendService getSuspendService() {
+        return suspendService;
+    }
+
+    public void setSuspendService(SuspendService suspendService) {
+        this.suspendService = suspendService;
+    }
+
+    @ManagedProperty(value = "#{suspendService}")
+    private SuspendService suspendService;
 
     public void startCheckingRegCertificate(){
         long id = Long.parseLong(recId);
@@ -55,4 +68,19 @@ public class TestReportsBn {
         this.prodApplicationsService = prodApplicationsService;
     }
 
+public void createLetter() {
+    long id = Long.parseLong(recId);
+    SuspDetail sd = suspendService.findSuspendDetail(id);
+
+    suspendService.setSuspDetail(sd, (long) 1);
+    try {
+        suspendService.createSuspLetter();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }  catch (JRException e) {
+        e.printStackTrace();
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+}
 }
