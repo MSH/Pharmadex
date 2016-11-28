660907afa38758a5205d201f28edb78c0e280685
okurasov <okurasov@24bb7120-ba24-2248-a177-eda5b6c43647>
2016-11-28 09:44:12 +0000

    dudchenko
    git-svn-id: https://www.etbmanager.org/svn/pharmadex/pharmadex/trunk@1196 24bb7120-ba24-2248-a177-eda5b6c43647

--- bangladesh/src/main/java/org/msh/pharmadex/mbean/product/ProcessProdBnBg.java
+++ bangladesh/src/main/java/org/msh/pharmadex/mbean/product/ProcessProdBnBg.java
@@ -11,8 +11,10 @@ import javax.faces.context.FacesContext;
 
 import org.msh.pharmadex.auth.UserSession;
 import org.msh.pharmadex.domain.ProdApplications;
+import org.msh.pharmadex.domain.Workspace;
 import org.msh.pharmadex.service.ProdApplicationsServiceAdd;
 import org.msh.pharmadex.service.ReviewService;
+import org.msh.pharmadex.util.JsfUtils;
 
 
 
@@ -24,110 +26,118 @@ import org.msh.pharmadex.service.ReviewService;
 @ManagedBean
 @ViewScoped
 public class ProcessProdBnBg implements Serializable {
-   
+
 
 	@ManagedProperty(value = "#{processProdBn}")
-    public ProcessProdBn processProdBn;
-    @ManagedProperty(value = "#{userSession}")
-    public UserSession userSession;
-    
-    @ManagedProperty(value = "#{prodApplicationsServiceAdd}")
-    public ProdApplicationsServiceAdd prodApplicationsServiceAdd;
-    @ManagedProperty(value = "#{reviewService}")
-    public ReviewService reviewService;
+	public ProcessProdBn processProdBn;
+	@ManagedProperty(value = "#{userSession}")
+	public UserSession userSession;
 
-    private String changedFields;
-    protected boolean displayVerify = false;
-   // private Logger logger = LoggerFactory.getLogger(ProcessProdBn.class);
-    //private JasperPrint jasperPrint;
-   private boolean showFeedBackButton;
-    private List<ProdApplications> allAncestors;
-    private String backTo = "";
-    private FacesContext facesContext = FacesContext.getCurrentInstance();
+	@ManagedProperty(value = "#{prodApplicationsServiceAdd}")
+	public ProdApplicationsServiceAdd prodApplicationsServiceAdd;
+	@ManagedProperty(value = "#{reviewService}")
+	public ReviewService reviewService;
 
-    @PostConstruct
-    private void init() {
-        //Long id = Scrooge.beanParam("Id");
-   	    ProdApplications pa= processProdBn.getProdApplications();
-        if (pa!=null)changedFields=pa.getAppComment();
-    	if (changedFields==null) changedFields="";
-    }
-    public boolean isFieldChanged(String fieldname){
-  	  //получим список из review_info.changedFields  если в списке нет, то false
-   //   	 String fieldname = (String) UIComponent.getCurrentComponent(FacesContext.getCurrentInstance()).getAttributes().get("fieldvalue");
-      	   
-      	if (changedFields.contains(fieldname)) return true;
-      	return false;
-      }
-      
-     public boolean findInnChanged(){
-    	  //получим список из review_info.changedFields  если в списке нет, то false
-         	if (changedFields.contains("inns")) return true;
-        	return false;
-      }
-     
-     public boolean findExcipientChanged(){
-  	  	  //получим список из review_info.changedFields  если в списке нет, то false
-  	       	if (changedFields.contains("excipients"))
-                  return true;
-              else
-  	      	    return false;
-  	    }
-     public boolean findAtcChanged(){
-  	  	  //получим список из review_info.changedFields  если в списке нет, то false
-  	       	if (changedFields.contains("Atc")) return true;
-  	      	return false;
-  	    }
-      
-     public boolean findCompaniesChanged(){
- 		if (changedFields.contains("ProdCompanies")) return true;
-       	return false;
-    }
-     
-     public void setShowFeedBackButton(boolean showFeedBackButton) {
-          this.showFeedBackButton = showFeedBackButton;
-      }
+	private String changedFields;
+	protected boolean displayVerify = false;
+	// private Logger logger = LoggerFactory.getLogger(ProcessProdBn.class);
+	//private JasperPrint jasperPrint;
+	private boolean showFeedBackButton;
+	private List<ProdApplications> allAncestors;
+	private String backTo = "";
+	private FacesContext facesContext = FacesContext.getCurrentInstance();
 
-      public List<ProdApplications> getAllAncestors(){
-          if (allAncestors==null){
-          ProdApplications prod = processProdBn.getProdApplications();
-          allAncestors = getProdApplicationsServiceAdd().getAllAncestor(prod);
-          }
-          return allAncestors;
-      }
+	@PostConstruct
+	private void init() {
+		//Long id = Scrooge.beanParam("Id");
+		ProdApplications pa= processProdBn.getProdApplications();
+		if (pa!=null)changedFields=pa.getAppComment();
+		if (changedFields==null) changedFields="";
+	}
+	public boolean isFieldChanged(String fieldname){
+		//получим список из review_info.changedFields  если в списке нет, то false
+		//   	 String fieldname = (String) UIComponent.getCurrentComponent(FacesContext.getCurrentInstance()).getAttributes().get("fieldvalue");
 
+		if (changedFields.contains(fieldname)) return true;
+		return false;
+	}
 
-      public void setAllAncestors(List<ProdApplications> allAncestors) {
-          this.allAncestors = allAncestors;
-      }
+	public boolean findInnChanged(){
+		//получим список из review_info.changedFields  если в списке нет, то false
+		if (changedFields.contains("inns")) return true;
+		return false;
+	}
 
-      public ProcessProdBn getProcessProdBn() {
-          return processProdBn;
-      }
+	public boolean findExcipientChanged(){
+		//получим список из review_info.changedFields  если в списке нет, то false
+		if (changedFields.contains("excipients"))
+			return true;
+		else
+			return false;
+	}
+	public boolean findAtcChanged(){
+		//получим список из review_info.changedFields  если в списке нет, то false
+		if (changedFields.contains("Atc")) return true;
+		return false;
+	}
 
-      public void setProcessProdBn(ProcessProdBn processProdBn) {
-          this.processProdBn = processProdBn;
-      }
+	public boolean findCompaniesChanged(){
+		if (changedFields.contains("ProdCompanies")) return true;
+		return false;
+	}
 
-      public UserSession getUserSession() {
-          return userSession;
-      }
+	public void setShowFeedBackButton(boolean showFeedBackButton) {
+		this.showFeedBackButton = showFeedBackButton;
+	}
 
-      public void setUserSession(UserSession userSession) {
-          this.userSession = userSession;
-      }
+	public List<ProdApplications> getAllAncestors(){
+		if (allAncestors==null){
+			ProdApplications prod = processProdBn.getProdApplications();
+			allAncestors = getProdApplicationsServiceAdd().getAllAncestor(prod);
+		}
+		return allAncestors;
+	}
 
-      public ProdApplicationsServiceAdd getProdApplicationsServiceAdd() {
-          return prodApplicationsServiceAdd;
-      }
+	/**
+	 * Issues #2339
+	 * Expiry date should be calculated as registration date + 365*5 (days)
+	 */
+	public void dateChange() {
+		int countDay = 365*5;
+		getProcessProdBn().getProdApplications().setRegExpiryDate(JsfUtils.addDays(getProcessProdBn().getProdApplications().getRegistrationDate(), countDay));
+	}
 
-      public void setProdApplicationsServiceAdd(ProdApplicationsServiceAdd prodApplicationsServiceNA) {
-          this.prodApplicationsServiceAdd = prodApplicationsServiceNA;
-      }
-      public ReviewService getReviewService() {
-  		return reviewService;
-  	}
-  	public void setReviewService(ReviewService reviewService) {
-  		this.reviewService = reviewService;
-  	}
+	public void setAllAncestors(List<ProdApplications> allAncestors) {
+		this.allAncestors = allAncestors;
+	}
+
+	public ProcessProdBn getProcessProdBn() {
+		return processProdBn;
+	}
+
+	public void setProcessProdBn(ProcessProdBn processProdBn) {
+		this.processProdBn = processProdBn;
+	}
+
+	public UserSession getUserSession() {
+		return userSession;
+	}
+
+	public void setUserSession(UserSession userSession) {
+		this.userSession = userSession;
+	}
+
+	public ProdApplicationsServiceAdd getProdApplicationsServiceAdd() {
+		return prodApplicationsServiceAdd;
+	}
+
+	public void setProdApplicationsServiceAdd(ProdApplicationsServiceAdd prodApplicationsServiceNA) {
+		this.prodApplicationsServiceAdd = prodApplicationsServiceNA;
+	}
+	public ReviewService getReviewService() {
+		return reviewService;
+	}
+	public void setReviewService(ReviewService reviewService) {
+		this.reviewService = reviewService;
+	}
 }
