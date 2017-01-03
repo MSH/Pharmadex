92475c79eceddabfddbefc6c903467983cfae3df
okurasov <okurasov@24bb7120-ba24-2248-a177-eda5b6c43647>
2017-01-03 14:22:05 +0000

    muskiv
    git-svn-id: https://www.etbmanager.org/svn/pharmadex/pharmadex/trunk@1239 24bb7120-ba24-2248-a177-eda5b6c43647

---
+++ bangladesh/src/main/java/org/msh/pharmadex/service/ManufacFP.java
@@ -0,0 +1,46 @@
+package org.msh.pharmadex.service;
+
+public class ManufacFP {
+	
+	private String productName;
+	private String companyName;
+	private String addr;	
+	private String appShelf;
+	private String storageCond;
+	
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	
+	public String getcompanyName() {
+		return companyName;
+	}
+	public void setcompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	
+	public String getaddr() {
+		return addr;
+	}
+	public void setaddr(String addr) {
+		this.addr = addr;
+	}
+	public String getStorageCond() {
+		return storageCond;
+	}
+	public void setStorageCond(String storageCond) {
+		this.storageCond = storageCond;
+	}
+	public String getAppShelf() {
+		return appShelf;
+	}
+	public void setAppShelf(String appShelf) {
+		this.appShelf = appShelf;
+	}
+	
+	
+
+}
