dccfa05d571366cb8d2ae86b14b714acded540a2
okurasov <okurasov@24bb7120-ba24-2248-a177-eda5b6c43647>
2017-02-02 08:58:28 +0000

    AK Format source
    git-svn-id: https://www.etbmanager.org/svn/pharmadex/pharmadex/trunk@1265 24bb7120-ba24-2248-a177-eda5b6c43647

--- core/src/main/java/org/msh/pharmadex/service/ProdAppChecklistService.java
+++ core/src/main/java/org/msh/pharmadex/service/ProdAppChecklistService.java
@@ -2,6 +2,7 @@ package org.msh.pharmadex.service;
 
 import org.msh.pharmadex.dao.iface.ProdAppChecklistDAO;
 import org.msh.pharmadex.domain.ProdAppChecklist;
+import org.msh.pharmadex.domain.enums.YesNoNA;
 import org.msh.pharmadex.util.RetObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,37 +16,60 @@ import java.util.List;
 @Service
 public class ProdAppChecklistService implements Serializable {
 
-    @Autowired
-    private ProdAppChecklistDAO prodAppChecklistDAO;
+	@Autowired
+	private ProdAppChecklistDAO prodAppChecklistDAO;
 
 
-    public List<ProdAppChecklist> findProdAppChecklistByProdApp(Long prodAppID) {
-        return prodAppChecklistDAO.findByProdApplications_IdOrderByIdAsc(prodAppID);
-    }
+	public List<ProdAppChecklist> findProdAppChecklistByProdApp(Long prodAppID) {
+		return prodAppChecklistDAO.findByProdApplications_IdOrderByIdAsc(prodAppID);
+	}
 
-    public RetObject saveProdAppChecklist(ProdAppChecklist prodAppChecklist) {
-        RetObject retObj = new RetObject();
-        try {
-            ProdAppChecklist chklist = prodAppChecklistDAO.saveAndFlush(prodAppChecklist);
-            retObj.setObj(prodAppChecklistDAO.findByProdApplications_IdOrderByIdAsc(chklist.getProdApplications().getId()));
-            retObj.setMsg("persist");
-        } catch (Exception ex) {
-            retObj.setMsg("error");
-            retObj.setObj(null);
-        }
-        return retObj;
-    }
+	public RetObject saveProdAppChecklist(ProdAppChecklist prodAppChecklist) {
+		RetObject retObj = new RetObject();
+		try {
+			ProdAppChecklist chklist = prodAppChecklistDAO.saveAndFlush(prodAppChecklist);
+			retObj.setObj(prodAppChecklistDAO.findByProdApplications_IdOrderByIdAsc(chklist.getProdApplications().getId()));
+			retObj.setMsg("persist");
+		} catch (Exception ex) {
+			retObj.setMsg("error");
+			retObj.setObj(null);
+		}
+		return retObj;
+	}
 
-    public RetObject saveProdAppChecklists(List<ProdAppChecklist> prodAppChecklists) {
-        RetObject retObj = new RetObject();
-        try {
-            List<ProdAppChecklist> chklist = prodAppChecklistDAO.save(prodAppChecklists);
-            retObj.setObj(chklist);
-            retObj.setMsg("persist");
-        } catch (Exception ex) {
-            retObj.setMsg("error");
-            retObj.setObj(null);
-        }
-        return retObj;
-    }
+	public RetObject saveProdAppChecklists(List<ProdAppChecklist> prodAppChecklists) {
+		RetObject retObj = new RetObject();
+		try {
+			List<ProdAppChecklist> chklist = prodAppChecklistDAO.save(prodAppChecklists);
+			retObj.setObj(chklist);
+			retObj.setMsg("persist");
+		} catch (Exception ex) {
+			retObj.setMsg("error");
+			retObj.setObj(null);
+		}
+		return retObj;
+	}
+	/**
+	 * Strict check screener's responses. All should be defined
+	 * @param prodAppChecklists
+	 * @return
+	 */
+	public boolean checkStrict(List<ProdAppChecklist> prodAppChecklists) {
+		if(prodAppChecklists != null){
+			for(ProdAppChecklist item : prodAppChecklists){
+				if(item.getStaffValue() != item.getValue()){
+					if(item.getStaffValue() != null){
+						if(item.getStaffValue() != item.getValue()){
+							return item.getStaffValue() != YesNoNA.NO;
+						}
+					}else{
+						return false;
+					}
+				}
+			}
+			return true;
+		}else{
+			return false;
+		}
+	}
 }
