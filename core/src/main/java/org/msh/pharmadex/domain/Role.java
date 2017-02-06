e94a56393f1e2e5e11e498fc1f76e2a636af4651
okurasov <okurasov@24bb7120-ba24-2248-a177-eda5b6c43647>
2017-02-06 10:48:01 +0000

    AK add hashCode and equals
    git-svn-id: https://www.etbmanager.org/svn/pharmadex/pharmadex/trunk@1274 24bb7120-ba24-2248-a177-eda5b6c43647

--- core/src/main/java/org/msh/pharmadex/domain/Role.java
+++ core/src/main/java/org/msh/pharmadex/domain/Role.java
@@ -61,4 +61,31 @@ public class Role extends CreationDetail implements Serializable {
     public void setDescription(String description) {
         this.description = description;
     }
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((roleId == null) ? 0 : roleId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (roleId == null) {
+			if (other.roleId != null)
+				return false;
+		} else if (!roleId.equals(other.roleId))
+			return false;
+		return true;
+	}
+    
+    
 }
