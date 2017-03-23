5df2a5f1da0ece294b483b288236ed1acfebe2d4
okurasov <okurasov@24bb7120-ba24-2248-a177-eda5b6c43647>
2017-03-23 15:14:04 +0000

    muskiv
    git-svn-id: https://www.etbmanager.org/svn/pharmadex/pharmadex/trunk@1380 24bb7120-ba24-2248-a177-eda5b6c43647

--- bangladesh/src/main/java/org/msh/pharmadex/service/Reviewer.java
+++ bangladesh/src/main/java/org/msh/pharmadex/service/Reviewer.java
@@ -5,7 +5,7 @@ public class Reviewer {
 	private String FIO;
 	private String updateDate;
 	private String accepted;
-	private String rejected;
+	/*private String rejected;*/
 	
 	public String getFIO() {
 		return FIO;
@@ -27,12 +27,12 @@ public class Reviewer {
 	public void setAccepted(String accepted) {
 		this.accepted = accepted;
 	}
-	
+	/*
 	public String getRejected() {
 		return rejected;
 	}
 	public void setRejected(String rejected) {
 		this.rejected = rejected;
-	}
+	}*/
 
 }
