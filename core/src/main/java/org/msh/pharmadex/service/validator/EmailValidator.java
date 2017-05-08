d97b81f0e1ef99b036c845dfdfc8a823996fc007
okurasov <okurasov@24bb7120-ba24-2248-a177-eda5b6c43647>
2017-05-08 14:34:37 +0000

    AK Right e-Mail pattern!
    git-svn-id: https://www.etbmanager.org/svn/pharmadex/pharmadex/trunk@1420 24bb7120-ba24-2248-a177-eda5b6c43647

--- core/src/main/java/org/msh/pharmadex/service/validator/EmailValidator.java
+++ core/src/main/java/org/msh/pharmadex/service/validator/EmailValidator.java
@@ -1,45 +1,87 @@
 package org.msh.pharmadex.service.validator;
 
-import org.msh.pharmadex.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
+import java.io.Serializable;
+import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import javax.el.ELContext;
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
-import java.io.Serializable;
-import java.util.ResourceBundle;
+
+import org.msh.pharmadex.auth.UserSession;
+import org.msh.pharmadex.domain.User;
+import org.msh.pharmadex.mbean.UserMBean;
+import org.springframework.stereotype.Component;
 
 /**
  * Author: usrivastava
  */
 @Component
+@Deprecated
 public class EmailValidator implements Validator, Serializable {
 
-    @Autowired
-    UserService userService;
+	private static final long serialVersionUID = -1927773446792527429L;
+	private Pattern pattern;
+	private Matcher matcher;
 
-    @Override
-    public void validate(FacesContext facesContext, UIComponent uiComponent, Object o) throws ValidatorException {
-        ResourceBundle resourceBundle = facesContext.getApplication().getResourceBundle(facesContext, "msgs");
+	private static final String EMAIL_PATTERN =
+			"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+					+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 
-        if(o==null){
-            FacesMessage msg = new FacesMessage(resourceBundle.getString("valid_value_req"));
-            msg.setSeverity(FacesMessage.SEVERITY_ERROR);
-            throw new ValidatorException(msg);
-        }
+	public EmailValidator() {
+		pattern = Pattern.compile(EMAIL_PATTERN);
 
-        String email = o.toString();
-//        if(userService.isEmailDuplicated(email)){
-//            FacesMessage msg = new FacesMessage(email
-//                    + resourceBundle.getString("valid_user_exist"));
-//            msg.setSeverity(FacesMessage.SEVERITY_ERROR);
-//            throw new ValidatorException(msg);
-//        }
+	}
 
-    }
+
+	/**
+	 * Rules:
+	 * <ul>
+	 * <li>Empty eMail allows only for Company user
+	 * <li>If email not empty, then structure of address should be checked unconditionally
+	 * </ul>
+	 */
+	@Override
+	public void validate(FacesContext facesContext, UIComponent uiComponent, Object o) throws ValidatorException {
+		ELContext elContext = FacesContext.getCurrentInstance().getELContext();
+		UserMBean userMBean
+		= (UserMBean) FacesContext.getCurrentInstance().getApplication()
+		.getELResolver().getValue(elContext, null, "userMBean");
+		ResourceBundle resourceBundle = facesContext.getApplication().getResourceBundle(facesContext, "msgs");
+		User selectedUser = null;
+		if(userMBean != null){
+			selectedUser = userMBean.getSelectedUser();
+		}
+
+		if(selectedUser != null){
+			if(o==null && selectedUser.isCompany()){
+				return;
+			}
+		}
+		if(o==null){
+			FacesMessage msg = new FacesMessage(resourceBundle.getString("email_is_empty"));
+			msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+			throw new ValidatorException(msg);
+		}
+
+		String email = o.toString().trim();
+		matcher = pattern.matcher(email);
+		if(!matcher.matches()){
+			FacesMessage msg = new FacesMessage(resourceBundle.getString("valid_email"));
+			msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+			throw new ValidatorException(msg);
+		}
+		//        if(userService.isEmailDuplicated(email)){
+		//            FacesMessage msg = new FacesMessage(email
+		//                    + resourceBundle.getString("valid_user_exist"));
+		//            msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+		//            throw new ValidatorException(msg);
+		//        }
+
+	}
 }
 
